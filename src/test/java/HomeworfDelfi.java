dba0126787e37f31994991d9decef92876e995b6
Pokahonta <kristine.tene@gmail.com>
2020-08-02 21:08:44 +0300

    Delfi (find all articles with comments, find first article with comment)

---
+++ src/test/java/HomeworfDelfi.java
@@ -0,0 +1,96 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.print.DocFlavor;
+import java.util.List;
+
+public class HomeworfDelfi {
+    private WebDriver driver;
+    //---Home Page---//
+    private final By ARTICLE_HP = By.tagName("article");
+    private final By TITLE_HP  = By.xpath(".//h1[contains(@class, 'headline__title')]");
+    private final By COMMENT_COUNTS_HP = By.xpath(".//a[contains(@class, 'comment-count')]");
+    //---Article  Page---//
+    private final By TITLE_AP = By.xpath(".//h1[contains(@class, 'd-inline')]");
+    private final By COMMENTS_COUNTS_AP = By.xpath(".//a[contains(@class, 'd-print')]");
+    //----Comments Page---//
+    private final By COMMENTS_CP = By.xpath(".//span[@class = 'type-cnt']");
+
+    @Test
+    public void delfiHomeTest() {
+
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://delfi.lv");
+
+    //----------------------Home Page----------------------//
+                  //find articles on Home Page
+        List<WebElement> articles = driver.findElements(ARTICLE_HP);
+
+                   //find 3rd article on Home Page
+        WebElement article = articles.get(2);
+
+                   //find article title on Home Page
+        String homePageTitle = article.findElement(TITLE_HP).getText();
+
+                    //find article comments count on Home Page
+        int homePageCommentsCount = 0;
+        if (!article.findElements(COMMENT_COUNTS_HP).isEmpty()){
+            homePageCommentsCount = parseCommentCount(article.findElement(COMMENT_COUNTS_HP).getText());
+        }
+        //System.out.println(homePageTitle + " " + homePageCommentsCount);
+
+                    //click on it
+        article.findElement(TITLE_HP).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(TITLE_AP));
+
+    //--------------------ARTICLE PAGE---------------------//
+                     //find title on Article Page
+        String articlesPageTitle = driver.findElement(TITLE_AP).getText();
+
+                     //find comments count on Article Page
+        int articlesPageCommentCount = parseCommentCount(driver.findElement(COMMENTS_COUNTS_AP).getText());
+
+
+                     //check
+        Assertions.assertTrue(homePageTitle.startsWith(articlesPageTitle), "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, articlesPageCommentCount, "Wrong comment count!");
+
+                      //click(open) on it
+        driver.findElement(COMMENTS_COUNTS_AP).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(TITLE_AP));
+
+    //------------------COMMENTS PAGE----------------------//
+                      //find title on Comments Page
+        String commentsPageTitle = driver.findElement(TITLE_AP).getText();
+
+                      //find comments on Comments Page
+        int commentsPageCommentCount = Integer.parseInt(driver.findElement(COMMENTS_CP).getText());
+
+                      //check
+
+        Assertions.assertTrue(homePageTitle.startsWith(commentsPageTitle), "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, commentsPageCommentCount, "Wrong comment count!");
+
+            }
+    private int parseCommentCount(String textToParse){
+        textToParse = textToParse.substring(1, textToParse.length() -1);
+        return Integer.parseInt(textToParse);
+
+            }
+    }
+
+
+
