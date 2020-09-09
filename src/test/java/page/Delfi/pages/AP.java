957554971cbf21eb75754a29e8e1848d09e8cf80
Pokahonta <kristine.tene@gmail.com>
2020-09-09 09:49:17 +0300

    Delfi Home Tasks (package, page)

---
+++ src/test/java/page/Delfi/pages/AP.java
@@ -0,0 +1,34 @@
+package page.Delfi.pages;
+
+import org.openqa.selenium.By;
+import page.Delfi.DelfiFunctions;
+import page.Delfi.helpers.CommentsHelper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AP {
+
+    private final By TITLE_AP = By.xpath(".//h1[contains(@class, 'd-inline')]");
+    private final By COMMENT_COUNT_AP = By.xpath(".//a[contains(@class, 'd-print')]");
+    private final By TIME = By.xpath(".//time[comtains(@class, 'd-block')]");
+
+    private DelfiFunctions df;
+
+    public AP(DelfiFunctions df) {
+        this.df = df;
+
+
+        assertTrue(df.isElementPresents(TIME), "There is no time on");
+    }
+    public String getTitle(){
+        return df.getText(TITLE_AP).trim();
+
+    }
+    public CP openComments(){
+        df.click(COMMENT_COUNT_AP);
+        return new CP(df);
+    }
+    public int getComments() {
+        return CommentsHelper.parseCommentCount(df.findElement(COMMENT_COUNT_AP).getText());
+    }
+}
